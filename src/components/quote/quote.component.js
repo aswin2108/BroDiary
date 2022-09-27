48bf014fa878e80e5ea51323e93e172c2c5003f0
aswins2108@gmail.com <aswins2108@gmail.com>
2022-09-27 16:46:15 +0530

    Basic layout of quote component

---
+++ src/components/quote/quote.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './quote.styles.css';
+
+const Quote=()=>(
+    <div className="quoteBox">
+    <div className="quote">
+    <h1>Success is almost totally dependent upon drive and persistence. The extra energy required to make another effort oe try another approach is secret of winning.</h1>
+   </div>
+   <div className="author">
+     <h2>:- Denis Waitley</h2>
+   </div>
+   </div>
+);
+
+export default Quote;
