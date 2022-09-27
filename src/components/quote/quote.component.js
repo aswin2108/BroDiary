f0664f323bacfd8e958e3749dabc02f6970e1864
aswins2108@gmail.com <aswins2108@gmail.com>
2022-09-27 18:05:46 +0530

    Fetching & displaying quote with button and on reload

--- src/components/quote/quote.component.js
+++ src/components/quote/quote.component.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import './quote.styles.css';
 
-const Quote=()=>(
+const Quote=()=>{
+    const [quotes, setQuotes] =useState("");
+    const getQuote=()=>{
+        fetch("https://type.fit/api/quotes")
+            .then((res)=>res.json())
+            .then((data)=>{
+                let randomNum=Math.floor(Math.random()*data.length);
+                setQuotes(data[randomNum]);
+            })
+    }
+
+    useEffect(()=>{
+        getQuote();
+    }, []);
+
+    return(
     <div className="quoteBox">
     <div className="quote">
-    <h1>Success is almost totally dependent upon drive and persistence. The extra energy required to make another effort oe try another approach is secret of winning.</h1>
+    <h1>"{quotes.text}"</h1>
    </div>
    <div className="author">
-     <h2>:- Denis Waitley</h2>
+     <h2>:- {quotes.author}</h2>
    </div>
+   <div className="quote-button-cont">
+       <button className="quote-button"  onClick={getQuote}>next</button>
+       </div>
    </div>
-);
+    );
+};
 
 export default Quote;
