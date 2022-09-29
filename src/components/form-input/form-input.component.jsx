2900abacef4799e3b927ce496b551f2d398cf64c
aswins2108@gmail.com <aswins2108@gmail.com>
2022-09-29 23:48:07 +0530

    styling of sign-up form completed

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,10 +1,20 @@
+import './form-input.styles.scss'
+
 const FormInput=({label,...otherProps})=>{
     return(
-        <div>
-        <label>{label}</label>
-        <input {...otherProps}/>
+        <div className="group">
+        <input className="form-input" {...otherProps}/>
+
+        {
+            label &&(
+               <label 
+                  className={`${otherProps.value.length ? 'shrink':''}
+                  form-input-label`}
+                  >{label}
+                </label>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default FormInput;
