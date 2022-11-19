81131cc1918bd3e1f7799eced9bc83ca0e93ec79
aswins2108@gmail.com <aswins2108@gmail.com>
2022-11-19 17:01:06 +0530

    Header links organised

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,28 +8,33 @@ import { signOutUser } from "../../firebase/firebase.utils";
 import './header.styles.css'
 
 const Header=()=>{
-  const {currentUser}=useContext(UserContext);
 
-    return(
-          <div className="navigation">
-            <Link className="logo-link" to='/'>
-              BroDiary
-            </Link>
-            <div className="link-cont">
-            {
-              currentUser ? (<div className="DL-cont">
-                <Link className="diary-link" to="/diary">DIARY</Link>
-                <span className="headder-link" onClick={signOutUser}>SIGN OUT</span>
-                </div>)
-                : (<Link className="headder-link" to='/auth'>
-                      SIGN IN
-                  </Link>
+  return(
+      <div className="navigation">
+        <Link className="logo-link" to='/'>
+          BroDiary
+        </Link>
+        <ShowLinks/>
+      </div>
+  );
+}
+
+function ShowLinks(){
+  const {currentUser}=useContext(UserContext);
+  return(
+    <div className="link-cont">
+        {
+          currentUser ? (<div className="DL-cont">
+              <Link className="diary-link" to="/diary">DIARY</Link>
+              <span className="headder-link" onClick={signOutUser}>SIGN OUT</span>
+             </div>)
+            : (<Link className="headder-link" to='/auth'>
+                 SIGN IN
+               </Link>
               )
-            }
-            
-            </div>
-          </div>
-    );
+        }     
+    </div>
+  )
 }
 
 export default Header;
